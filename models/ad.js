5684c0cece4f1a817214bb493f8dc574eacf5a28
lenaabdu <lenaabdu23@gmail.com>
2020-09-20 23:31:58 +0100

    first commit

--- models/ad.js
+++ models/ad.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const adSchema = mongoose.Schema({
-    name: String,
-    price: Number,
-    location: String,
-    date: String
+    title: String,
+    Date: String,
+    description: String,
+    markdown: String,
 })
 
 module.exports.Ad = mongoose.model('Ad', adSchema)
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const { User } = require('../models/user');
 //const dburi =
 
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/adsdatabase', {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/database', {
 
     userNewUrlParser: true,
     useUnifiedTopology: true
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/adsdatabase', {
 
 // defining the Express app
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3004
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
